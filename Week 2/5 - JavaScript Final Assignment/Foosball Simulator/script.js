d6d59171a7d9b0c40ef1f34cc2694afdb8012eb1
Charchit <charchitregmi10@gmail.com>
2023-12-05 21:51:20 +0545

    Minor changes in Assignment Table

--- Week 2/5 - JavaScript Final Assignment/Foosball Simulator/script.js
+++ Week 2/5 - JavaScript Final Assignment/Foosball Simulator/script.js
@@ -1,7 +1,7 @@
 // Setting up the ground
 let ground;
-let groundWidth = 1320;   // Background image has 1320p width
-let groundHeight = 660;  // Background image has 660p height
+let groundWidth = 1320;
+let groundHeight = 660;
 
 // Get the canvas element by its ID
 ground = document.getElementById('ground');
@@ -36,22 +36,64 @@ ctx.fillRect(groundWidth - goalPostOffset - goalPostWidth, (groundHeight - goalP
 
 // Setting up the players
 // Define player column properties
-let columnWidth = 100; // Width of each column
+let columnWidth = 20; // Width of each player (column)
 let columnGap = 48;    // Gap between columns
 let columnCount = 4;   // Number of columns
-let playerHeight = 50; // Height of each player
+let playerHeight = 30; // Height of each player
 
 // Calculate total width occupied by columns and gaps
 let totalColumnsWidth = columnCount * columnWidth + (columnCount - 1) * columnGap;
 
-// Calculate the starting position for the columns
-let startX = (groundWidth / 2 - totalColumnsWidth) / 2; // Adjust for left side
+// Calculate the starting position for the columns on the left side
+let startXLeft = (groundWidth / 2 - totalColumnsWidth) / 2;
 
-// Draw four columns of players
+/*
+// Left side team (sky blue team)
+column for gk: 1 player
+column for DF: 3 players
+column for MID: 4 players
+column for FWD: 3 players 
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Draw four columns of sky blue players on the left side
 for (let i = 0; i < columnCount; i++) {
-    let columnX = startX + i * (columnWidth + columnGap);
+    let columnX = startXLeft + i * (columnWidth + columnGap);
 
-    // Draw a player rectangle in each column
-    ctx.fillStyle = 'blue'; // Change color or style as needed
+    // Draw a sky blue player rectangle in each column on the left side
+    ctx.fillStyle = 'skyblue';
+    ctx.fillRect(columnX, (groundHeight - playerHeight) / 2, columnWidth, playerHeight);
+}
+
+// Calculate the starting position for the columns on the right side
+let startXRight = groundWidth / 2 + (groundWidth / 2 - totalColumnsWidth) / 2;
+
+// Draw four columns of red players on the right side
+for (let i = 0; i < columnCount; i++) {
+    let columnX = startXRight + i * (columnWidth + columnGap);
+
+    // Draw a red player rectangle in each column on the right side
+    ctx.fillStyle = 'red';
     ctx.fillRect(columnX, (groundHeight - playerHeight) / 2, columnWidth, playerHeight);
 }
