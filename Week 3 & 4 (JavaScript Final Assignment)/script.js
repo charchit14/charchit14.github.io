c573d3c0323d84423e547cec42ad42a0c672352e
Charchit <charchitregmi10@gmail.com>
2023-12-10 19:17:16 +0545

    Goal post collision

--- Week 3 & 4 (JavaScript Final Assignment)/script.js
+++ Week 3 & 4 (JavaScript Final Assignment)/script.js
@@ -125,12 +125,12 @@ var Game = {
   update: function () {
     if (!this.over) {
       // If the ball collides with the bound limits - correct the x and y coords.
-      if (this.ball.x <= 0) Foosball._resetTurn.call(this, this.ai, this.player);
-      if (this.ball.x >= this.canvas.width - this.ball.width)
-        Foosball._resetTurn.call(this, this.player, this.ai);
-      if (this.ball.y <= 0) this.ball.moveY = DIRECTION.DOWN;
-      if (this.ball.y >= this.canvas.height - this.ball.height)
-        this.ball.moveY = DIRECTION.UP;
+      // if (this.ball.x <= 0) Foosball._resetTurn.call(this, this.ai, this.player);
+      // if (this.ball.x >= this.canvas.width - this.ball.width)
+      //   Foosball._resetTurn.call(this, this.player, this.ai);
+      // if (this.ball.y <= 0) this.ball.moveY = DIRECTION.DOWN;
+      // if (this.ball.y >= this.canvas.height - this.ball.height)
+      //   this.ball.moveY = DIRECTION.UP;
 
       // Move player if they player.move value was updated by a keyboard event
       if (this.player.move === DIRECTION.UP) this.player.y -= this.player.speed;
@@ -209,6 +209,41 @@ var Game = {
         }
       }
     }
+
+
+    // BELOW THIS
+
+// If the ball collides with the bound limits - correct the x and y coords.
+    if (this.ball.x <= 0) Foosball._resetTurn.call(this, this.ai, this.player);
+    if (this.ball.x >= this.canvas.width - this.ball.width)
+        Foosball._resetTurn.call(this, this.player, this.ai);
+    if (this.ball.y <= 0) this.ball.moveY = DIRECTION.DOWN;
+    if (this.ball.y >= this.canvas.height - this.ball.height)
+        this.ball.moveY = DIRECTION.UP;
+
+    // Add collision detection with goal posts
+    const goalPostWidth = 40; // Width of the goal posts
+    const goalPostGap = 20; // Gap between the goal posts and D-box
+
+    // Left goal post collision
+    if (this.ball.x <= goalPostWidth && this.ball.y >= groundMidPoint - dBoxHeight / 2 - goalPostGap && this.ball.y <= groundMidPoint + dBoxHeight / 2 + goalPostGap) {
+        // Ball hit the left goal post
+        this.player.score++; // Increment player's goal count
+        this._resetTurn(this.ai, this.player); // Trigger next turn
+    }
+
+    // Right goal post collision
+    if (this.ball.x >= this.canvas.width - goalPostWidth - this.ball.width && this.ball.y >= groundMidPoint - dBoxHeight / 2 - goalPostGap && this.ball.y <= groundMidPoint + dBoxHeight / 2 + goalPostGap) {
+        // Ball hit the right goal post
+        this.ai.score++; // Increment AI's goal count
+        this._resetTurn(this.player, this.ai); // Trigger next turn
+    }
+
+
+    // ABOVE THIS
+
+
+
 
     // Handle the end of round transition
     // Check to see if the player won the round.
@@ -442,13 +477,37 @@ var Game = {
   },
 
   // Reset the ball location, the player turns and set a delay before the next round begins.
+  // _resetTurn: function (winner, loser) {
+  //   this.ball = Ball.new.call(this, this.ball.speed);
+  //   this.turn = loser;
+  //   this.timer = new Date().getTime();
+
+  //   winner.score++;
+  // },  
+
+
+  // NEW
   _resetTurn: function (winner, loser) {
     this.ball = Ball.new.call(this, this.ball.speed);
     this.turn = loser;
     this.timer = new Date().getTime();
-
     winner.score++;
   },
+  // END NEW
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   // Wait for a delay to have passed after each turn.
   _turnDelayIsOver: function () {
@@ -465,34 +524,3 @@ var Game = {
 
 var Foosball = Object.assign({}, Game);
 Foosball.initialize();
-
-
-
-
-
-// Goal detection for post collision
-// If the ball collides with the bound limits - correct the x and y coords.
-// if (this.ball.x <= 0) Foosball._resetTurn.call(this, this.ai, this.player);
-// if (this.ball.x >= this.canvas.width - this.ball.width)
-//     Foosball._resetTurn.call(this, this.player, this.ai);
-// if (this.ball.y <= 0) this.ball.moveY = DIRECTION.DOWN;
-// if (this.ball.y >= this.canvas.height - this.ball.height)
-//     this.ball.moveY = DIRECTION.UP;
-
-// // Add collision detection with goal posts
-// const goalPostWidth = 40; // Width of the goal posts
-// const goalPostGap = 20; // Gap between the goal posts and D-box
-
-// // Left goal post collision
-// if (this.ball.x <= goalPostWidth && this.ball.y >= groundMidPoint - dBoxHeight / 2 - goalPostGap && this.ball.y <= groundMidPoint + dBoxHeight / 2 + goalPostGap) {
-//     // Ball hit the left goal post
-//     // Increment goal count or trigger goal event
-//     // ...
-// }
-
-// // Right goal post collision
-// if (this.ball.x >= this.canvas.width - goalPostWidth - this.ball.width && this.ball.y >= groundMidPoint - dBoxHeight / 2 - goalPostGap && this.ball.y <= groundMidPoint + dBoxHeight / 2 + goalPostGap) {
-//     // Ball hit the right goal post
-//     // Increment goal count or trigger goal event
-//     // ...
-// }
