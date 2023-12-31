efa51aff6da51bf89d8d289f7dc80362fbc75384
Charchit <charchitregmi10@gmail.com>
2023-12-31 20:48:52 +0545

    Changes in files

--- Week 5/1 - To Do List Using TypeScript/src/event.ts
+++
@@ -1,6 +0,0 @@
-// // Event listener for the "Create Task" button
-// document.getElementById("create-task").addEventListener("click", function() {
-    
-//     // Call createTask function from TypeScript file
-//     createTask();
-// });
