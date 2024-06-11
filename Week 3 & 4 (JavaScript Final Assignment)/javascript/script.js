f7577653d7209312687c3fc347a136286eefbaf4
Charchit <charchitregmi10@gmail.com>
2024-06-11 14:20:56 +0545

    Increasing the speed of AI

--- Week 3 & 4 (JavaScript Final Assignment)/javascript/script.js
+++ Week 3 & 4 (JavaScript Final Assignment)/javascript/script.js
@@ -17,7 +17,7 @@ var dy = -2;
 var m = 0;
 var j = 0;
 
-var aiSpeed = 0.5; // CPU players movement speed
+var aiSpeed = 2; // CPU players movement speed
 
 // Setting up the paddle dimensions
 var paddleHeight = 10;
